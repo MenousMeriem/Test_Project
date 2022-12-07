f474fbfa18bbbc625fb021984afef0c6597ce68c
MenousMeriem <meriemmenous1@gmail.com>
2022-12-07 08:35:21 +0100

    first commit

---
+++ fichier.py
@@ -0,0 +1,26 @@
+
+print("Hello ")
+
+#print("hello ")
+
+""""print("Hello")
+print("Hello")
+print("Hello ")"""
+
+a = 5 
+b = 6 
+S = a+b
+print ("La somme des deux nombres est " +str(S))
+
+
+print ("Tapez le premier nombre :")
+a = input()
+
+print ("Tapez le deuxieme nombre :")
+b = input()
+
+S = int(a) + int(b)
+
+#print("La somme des deux nombres "+str(a)+"et"+str(b)+" est " +str(S))
+ 
+print("La somme des deux nombres ",a, "et" ,b, " est " ,S)
